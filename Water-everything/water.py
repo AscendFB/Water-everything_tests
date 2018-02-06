25ea2e4177276c711318ba29e7b2fa274850a2bd
AscendFB <hehe1234@hotmail.de>
2018-02-06 15:47:35 +0100

    ...

--- Water-everything/water.py
+++ Water-everything/water.py
@@ -19,31 +19,44 @@ def log(message, message_type):
     requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
                     data=payload, headers=headers)
 
-def api_get(endpoint):
-            """GET from an API endpoint."""
-            response = requests.get(self.api_url + endpoint, headers={
-            'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-            'content-type': "application/json"})
-            #self.api_response_error_collector(response)
-            #self.api_response_error_printer()
-            return response
+class test():
+    def init(self):
+        API_TOKEN = os.environ['API_TOKEN']
+        headers = {'Authorization': 'Bearer {}'.format(API_TOKEN),
+                            'content-type': "application/json"}
+        encoded_payload = API_TOKEN.split('.')[1]
+        encoded_payload += '=' * (4 - len(encoded_payload) % 4)
+        json_payload = base64.b64decode(encoded_payload).decode('utf-8')
+        server = json.loads(json_payload)['iss']
+        self.api_url = 'http{}:{}/api/'.format(
+                's' if not any([h in server for h in ['localhost', '192.168.']])
+                else '', server)
+
+    def api_get(self, endpoint):
+                """GET from an API endpoint."""
+                response = requests.get(self.api_url + endpoint, headers={
+                'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+                'content-type': "application/json"})
+                #self.api_response_error_collector(response)
+                #self.api_response_error_printer()
+                return response
 
 
-def load_plants_from_web_app():
-        """Download known plants from the FarmBot Web App API."""
-        response = api_get('points')
-        app_points = response.json()
-        if response.status_code == 200:
-            log('nice','info')
-            plants = []
-            for point in app_points:
-                if point['pointer_type'] == 'Plant':
-                    plants.append({
-                        'x': point['x'],
-                        'y': point['y'],})
-            self.plants['known'] = plants
-            self.sorted_coords = sorted(self.plants['known'])
-            log('sorted', 'info')
+    def load_plants_from_web_app(self):
+            """Download known plants from the FarmBot Web App API."""
+            response = self.api_get('points')
+            app_points = response.json()
+            if response.status_code == 200:
+                log('nice','info')
+                plants = []
+                for point in app_points:
+                    if point['pointer_type'] == 'Plant':
+                        plants.append({
+                            'x': point['x'],
+                            'y': point['y'],})
+                self.plants['known'] = plants
+                self.sorted_coords = sorted(self.plants['known'])
+                log('sorted', 'info')
 
 if __name__ == '__main__':
 
