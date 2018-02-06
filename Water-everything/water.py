e297cc7a060e4bec65f1d79829a7b40b54c828f4
AscendFB <hehe1234@hotmail.de>
2018-02-06 15:34:52 +0100

    ...

--- Water-everything/water.py
+++ Water-everything/water.py
@@ -19,6 +19,31 @@ def log(message, message_type):
     requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
                     data=payload, headers=headers)
 
+def api_get(self, endpoint):
+            """GET from an API endpoint."""
+            response = requests.get(self.api_url + endpoint, headers=self.headers)
+            #self.api_response_error_collector(response)
+            #self.api_response_error_printer()
+            return response
+
+
+def load_plants_from_web_app(self):
+        """Download known plants from the FarmBot Web App API."""
+        response = self.api_get('points')
+        app_points = response.json()
+        if response.status_code == 200:
+            plants = []
+            for point in app_points:
+                if point['pointer_type'] == 'Plant':
+                    plants.append({
+                        'x': point['x'],
+                        'y': point['y'],})
+            self.plants['known'] = plants
+            self.sorted_coords = sorted(self.plants['known'])
+            log('sorted', 'info')
+
 if __name__ == '__main__':
 
     log('test','info')
+    load_plants_from_web_app()
+    
