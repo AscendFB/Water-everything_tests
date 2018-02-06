fc01ae9179c1a1c9c00e2a5381b0107f74231ed8
AscendFB <hehe1234@hotmail.de>
2018-02-06 15:15:00 +0100

    ..

--- Water-everything/water.py
+++ Water-everything/water.py
@@ -12,10 +12,6 @@ from sequence_writer import Sequence
 #API_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1bmtub3duIiwic3ViIjoxNywiaWF0IjoxNTE3OTA1MTExLCJqdGkiOiI4NDdmNGVjMC03ZmZjLTQ4MzItODJmMy1hZjUwYThlMDMwYmYiLCJpc3MiOiIvL215LmZhcm1ib3QuaW86NDQzIiwiZXhwIjoxNTIxMzYxMTExLCJtcXR0IjoiYnJpc2stYmVhci5ybXEuY2xvdWRhbXFwLmNvbSIsImJvdCI6ImRldmljZV8xNyIsInZob3N0IjoidmJ6Y3hzcXIiLCJtcXR0X3dzIjoid3NzOi8vYnJpc2stYmVhci5ybXEuY2xvdWRhbXFwLmNvbTo0NDMvd3MvbXF0dCIsIm9zX3VwZGF0ZV9zZXJ2ZXIiOiJodHRwczovL2FwaS5naXRodWIuY29tL3JlcG9zL2Zhcm1ib3QvZmFybWJvdF9vcy9yZWxlYXNlcy9sYXRlc3QiLCJpbnRlcmltX2VtYWlsIjoiaGVoZTEyMzRAaG90bWFpbC5kZSIsImZ3X3VwZGF0ZV9zZXJ2ZXIiOiJERVBSRUNBVEVEIiwiYmV0YV9vc191cGRhdGVfc2VydmVyIjoiaHR0cHM6Ly9hcGkuZ2l0aHViLmNvbS9yZXBvcy9GYXJtQm90L2Zhcm1ib3Rfb3MvcmVsZWFzZXMvOTUxMDc2NSJ9.mHUQLkKLOsw4UXMwpq7Yl-45NRFeuP0mIw3o-c1aSYTY7hjLPydsam9kOGULf04hhsJEOdBdUtjOGcXpftbKtnZ1pSNbftRNfUKK8Vth7r94jd1l4-SCF7Oz7P4OxDCBAFOQqu3CM3Rr-kXKm_Mkw_qI2XYto8GATtLSyAby6ZU1ci-WHikfYJpSMZj7LZGDfiqPNbqV6OphmllktZUeE88ji3Qxn7dhNsTYaYKC3d-d_rlRB0J7cP-YEwUqpcU1qcWvHh_yT5AaUjtTYjOqjenbzqK9qvKew9p2CqWqMKuSUrxcsSaHQJBia1iKEdVnCQVNtsOJ39m-wQNc8Ie-Qg'
 
 
-def api_url():
-                major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
-                base_url = os.environ['FARMWARE_URL']
-                return base_url + 'api/v1/' if major_version > 5 else base_url
 
 
 class Water_everything():
@@ -32,20 +28,33 @@ class Water_everything():
             self.seq_id_as_int = 0  
             self.sequences = []
             self.water_sequence = []
+            
+            try:
+                api_token = os.environ['API_TOKEN']
+      #      except KeyError:
+     #           api_token = 'x.eyJpc3MiOiAiLy9zdGFnaW5nLmZhcm1ib3QuaW86NDQzIn0.x'
+      #          api_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1bmtub3duIiwic3ViIjoxNywiaWF0IjoxNTE3OTA1MTExLCJqdGkiOiI4NDdmNGVjMC03ZmZjLTQ4MzItODJmMy1hZjUwYThlMDMwYmYiLCJpc3MiOiIvL215LmZhcm1ib3QuaW86NDQzIiwiZXhwIjoxNTIxMzYxMTExLCJtcXR0IjoiYnJpc2stYmVhci5ybXEuY2xvdWRhbXFwLmNvbSIsImJvdCI6ImRldmljZV8xNyIsInZob3N0IjoidmJ6Y3hzcXIiLCJtcXR0X3dzIjoid3NzOi8vYnJpc2stYmVhci5ybXEuY2xvdWRhbXFwLmNvbTo0NDMvd3MvbXF0dCIsIm9zX3VwZGF0ZV9zZXJ2ZXIiOiJodHRwczovL2FwaS5naXRodWIuY29tL3JlcG9zL2Zhcm1ib3QvZmFybWJvdF9vcy9yZWxlYXNlcy9sYXRlc3QiLCJpbnRlcmltX2VtYWlsIjoiaGVoZTEyMzRAaG90bWFpbC5kZSIsImZ3X3VwZGF0ZV9zZXJ2ZXIiOiJERVBSRUNBVEVEIiwiYmV0YV9vc191cGRhdGVfc2VydmVyIjoiaHR0cHM6Ly9hcGkuZ2l0aHViLmNvbS9yZXBvcy9GYXJtQm90L2Zhcm1ib3Rfb3MvcmVsZWFzZXMvOTUxMDc2NSJ9.mHUQLkKLOsw4UXMwpq7Yl-45NRFeuP0mIw3o-c1aSYTY7hjLPydsam9kOGULf04hhsJEOdBdUtjOGcXpftbKtnZ1pSNbftRNfUKK8Vth7r94jd1l4-SCF7Oz7P4OxDCBAFOQqu3CM3Rr-kXKm_Mkw_qI2XYto8GATtLSyAby6ZU1ci-WHikfYJpSMZj7LZGDfiqPNbqV6OphmllktZUeE88ji3Qxn7dhNsTYaYKC3d-d_rlRB0J7cP-YEwUqpcU1qcWvHh_yT5AaUjtTYjOqjenbzqK9qvKew9p2CqWqMKuSUrxcsSaHQJBia1iKEdVnCQVNtsOJ39m-wQNc8Ie-Qg'
+            try:
+                encoded_payload = api_token.split('.')[1]
+                encoded_payload += '=' * (4 - len(encoded_payload) % 4)
+                json_payload = base64.b64decode(encoded_payload).decode('utf-8')
+                server = json.loads(json_payload)['iss']
+            except:  # noqa pylint:disable=W0702
+                server = '//my.farmbot.io'
+            self.api_url = 'http{}:{}/api/'.format(
+                's' if not any([h in server for h in ['localhost', '192.168.']])
+                else '', server)
+            self.headers = {'Authorization': 'Bearer {}'.format(api_token),
+                            'content-type': "application/json"}
+            self.errors = {}
 
-    def api_url(self):
-                major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
-                base_url = os.environ['FARMWARE_URL']
-                return base_url + 'api/v1/' if major_version > 5 else base_url
+
 
 
     def post(self, wrapped_data):
                 """Send the Celery Script command."""
-                self.headers = {
-                    'Authorization': 'bearer {}'.format(FARMWARE_TOKEN),
-                    'content-type': "application/json"}
                 payload = json.dumps(wrapped_data)
-                requests.post(api_url() + 'celery_script',
+                requests.post(self.api_url + 'celery_script',
                               data=payload, headers=self.headers)
 
     def log(self, message, message_type):
@@ -60,9 +69,7 @@ class Water_everything():
 
     def api_get(self, endpoint):
             """GET from an API endpoint."""
-            response = requests.get(api_url() + endpoint, headers={
-                    'Authorization': 'bearer {}'.format(FARMWARE_TOKEN),
-                    'content-type': "application/json"})
+            response = requests.get(self.api_url + endpoint, headers=self.headers)
             #self.api_response_error_collector(response)
             #self.api_response_error_printer()
             return response
@@ -118,7 +125,7 @@ class Water_everything():
                        print ("moving to points")
                        data = {"kind": "move_absolute", "args": {'x': plant['x'], 'y': plant['y'], 'z': 0, 'speed': 800}}
                        moving_coords = json.dumps(data)
-                       r = requests.post(api_url() + 'celery_script', data=moving_coords, headers= self.headers)
+                       r = requests.post(self.api_url + 'celery_script', data=moving_coords, headers= self.headers)
                        #CeleryPy.move_absolute(
                        # location=[plant['x'],plant['y'] ,0],
                        # offset=[0, 0, 0],
@@ -137,7 +144,7 @@ class Water_everything():
     def create_sequence(self):
 
             def upload(sequence):
-                r = requests.post(api_url() + 'sequences', data=json.dumps(sequence), headers=self.headers)
+                r = requests.post(self.api_url + 'sequences', data=json.dumps(sequence), headers=self.headers)
                 print(r, r.json())
                 self.response = r
 
